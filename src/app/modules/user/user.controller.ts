b4b879c1c2307f63099b6a4c765886c9857ef739
MukterHossain <mukter3075@gmail.com>
2025-11-03 23:01:56 +0600

    create user

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,15 +2,17 @@ import { Request, Response } from "express";
 import catchAsync from "../../shared/catchAsync";
 import sendResponse from "../../shared/sendResponse";
 import httpStatus from "http-status";
+import { UserService } from "./user.service";
 
 const createUser = catchAsync(async (req: Request, res: Response) => {
     // Your logic to create a user
-
+   const result = await UserService.createUser(req)
+    console.log("result Controller", result)
    sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: "User created successfully",
-    data: null
+    data: result ,
    })
   });
 
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,9 +1,22 @@
+import { Request } from "express"
+import bcrypt from "bcryptjs";
+import config from "../../../config";
+import { Prisma, User } from "@prisma/client";
+import { prisma } from "../../shared/prisma";
 
 
 
-const createUser = async () => {
-    // Your logic to create a user
-console.log("data")
+const createUser = async (req:Request) => {
+    const hashedPassword = await bcrypt.hash(req.body.password, Number(config.salt_round))
+
+    const result = await prisma.user.create({
+        data:{
+            email: req.body.email,
+            password: hashedPassword,
+        }
+    })
+    console.log("result", result)
+    return result;
 
 }
 
